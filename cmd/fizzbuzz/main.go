582249547874fc1d4f6d0247478e8ecc0873b716
xpetit <32063953+xpetit@users.noreply.github.com>
2022-01-14 19:32:33 +0100

    Refactor: move HTTP handlers in a separate package to improve reusability

--- cmd/fizzbuzz/main.go
+++ cmd/fizzbuzz/main.go
@@ -2,105 +2,15 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"os/signal"
-	"strconv"
-	"sync"
 	"syscall"
 	"time"
 
-	"github.com/xpetit/fizzbuzz"
+	"github.com/xpetit/fizzbuzz/handlers"
 )
-
-// stats holds a protected (thread safe) hit count
-type stats struct {
-	sync.RWMutex
-	m map[fizzbuzz.Config]int
-}
-
-// setInt sets the int pointed to by p to the value found in the values, or a default value.
-// It returns an error if the value found in the values cannot be parsed as an int or
-// if the resulting value is not strictly positive.
-func setInt(values url.Values, key string, defaultValue int, p *int) error {
-	if values.Has(key) {
-		i, err := strconv.Atoi(values.Get(key))
-		if err != nil {
-			err := err.(*strconv.NumError)
-			return fmt.Errorf("parsing %s %q: %w", key, err.Num, err.Err)
-		}
-		if i < 1 {
-			return fmt.Errorf("setting %s to %d: must be strictly positive", key, i)
-		}
-		*p = i
-	} else {
-		*p = defaultValue
-	}
-	return nil
-}
-
-// setString sets the string pointed to by p to the value found in the values, or a default value.
-func setString(values url.Values, key, value string, p *string) {
-	if values.Has(key) {
-		*p = values.Get(key)
-	} else {
-		*p = value
-	}
-}
-
-// handleFizzBuzz is an HTTP handler that answers with a JSON array containing the Fizz buzz values.
-// It accepts optional URL query parameters to change the default config.
-func (s *stats) handleFizzBuzz(rw http.ResponseWriter, r *http.Request) {
-	// Parse query parameters with default values
-	var c fizzbuzz.Config
-	values := r.URL.Query()
-	if err := setInt(values, "limit", 10, &c.Limit); err != nil {
-		http.Error(rw, err.Error(), http.StatusBadRequest)
-		return
-	} else if err := setInt(values, "int1", 2, &c.X); err != nil {
-		http.Error(rw, err.Error(), http.StatusBadRequest)
-		return
-	} else if err := setInt(values, "int2", 3, &c.Y); err != nil {
-		http.Error(rw, err.Error(), http.StatusBadRequest)
-		return
-	}
-	setString(values, "str1", "fizz", &c.A)
-	setString(values, "str2", "buzz", &c.B)
-
-	// Update fizzbuzz stats
-	s.Lock()
-	if s.m == nil {
-		s.m = map[fizzbuzz.Config]int{}
-	}
-	s.m[c]++
-	s.Unlock()
-
-	// Write fizzbuzz
-	c.WriteTo(rw)
-}
-
-// handleStats is an HTTP handler that answers with a JSON object describing the most frequent Fizz buzz config
-// If no subsequent call to fizzbuzz has been made, the "most_frequent" object is null.
-func (s *stats) handleStats(rw http.ResponseWriter, r *http.Request) {
-	var result struct {
-		MostFrequent *fizzbuzz.Config `json:"most_frequent"`
-		Count        int              `json:"count"`
-	}
-	s.RLock()
-	for f, count := range s.m {
-		if count > result.Count {
-			result.Count = count
-			f := f // shallow copy
-			result.MostFrequent = &f
-		}
-	}
-	s.RUnlock()
-	json.NewEncoder(rw).Encode(result)
-}
 
 func main() {
 	// Setup signal handler
@@ -121,13 +31,13 @@ func main() {
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  120 * time.Second,
-		Handler:      logger(log.Default())(api),
+		Handler:      handlers.Logger(log.Default())(api),
 	}
 
 	// Configure HTTP routing
-	var s stats
-	api.HandleFunc("/v1/fizzbuzz", s.handleFizzBuzz)
-	api.HandleFunc("/v1/statistics", s.handleStats)
+	var s handlers.Stats
+	api.HandleFunc("/v1/fizzbuzz", s.HandleFizzBuzz)
+	api.HandleFunc("/v1/statistics", s.HandleStats)
 
 	// Spawn a goroutine that waits for a termination signal and then stops the HTTP server
 	go func() {
